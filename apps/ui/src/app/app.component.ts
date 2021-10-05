34ef376d310d3b29da7527d277ead6254d991272
Josh Wright <jwwrightcalpoly@gmail.com>
2021-10-05 08:42:26 -0600

    initial commit

---
+++ apps/ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'known-unknowns-multiple-exemplar-experiment-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ui';
+}
