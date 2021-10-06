57b3f6c1f1a2523ba538bfdc1849df6423b255db
Josh Wright <jwwrightcalpoly@gmail.com>
2021-10-06 13:47:13 -0600

    improve form styling

--- apps/ui/src/app/study-form/study-form.module.ts
+++ apps/ui/src/app/study-form/study-form.module.ts
@@ -2,7 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -20,7 +22,9 @@ import { StudyFormComponent } from './study-form.component';
     MatCheckboxModule,
     MatInputModule,
     MatButtonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatCardModule,
+    MatDividerModule
   ],
   exports: [
     StudyFormComponent
