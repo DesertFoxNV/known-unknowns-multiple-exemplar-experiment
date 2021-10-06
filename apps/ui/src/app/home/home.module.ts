612ff4c1621284c74ea64a15c186e948e5051dc4
Josh Wright <jwwrightcalpoly@gmail.com>
2021-10-06 08:04:32 -0600

    added home module, fixed routing, and cert

--- apps/ui/src/app/home/home.module.ts
+++ apps/ui/src/app/home/home.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { HomeRoutingModule } from './home-routing.module';
-
+import { HomeComponent } from './home.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
+  exports: [HomeComponent]
 })
-export class HomeModule { }
+export class HomeModule {}
