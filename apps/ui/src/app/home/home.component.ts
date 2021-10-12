d5b9aaf12d925abe260668867337b30ac752da3e
Josh Wright <jwwrightcalpoly@gmail.com>
2021-10-11 20:59:37 -0600

    removed home component console logs

--- apps/ui/src/app/home/home.component.ts
+++ apps/ui/src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { FullySpecifiedNetwork, KnownUnknownNetwork } from '../study-conditions/network';
-import { StimulusCase } from '../study-conditions/stimulus-case';
 
 @Component({
   selector: 'known-unknowns-multiple-exemplar-experiment-home',
@@ -8,13 +6,4 @@ import { StimulusCase } from '../study-conditions/stimulus-case';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-}
-
-// for (const num of [1, 2, 3, 4, 5, 6, 7, 8, 9]) {
-//   const network = new FullySpecifiedNetwork(num, StimulusCase.upper);
-// }
-
-for (const num of [1, 2, 3, 4, 5, 6, 7, 8, 9]) {
-  const network = new KnownUnknownNetwork(num, StimulusCase.upper);
-  console.log(network.toString());
 }
