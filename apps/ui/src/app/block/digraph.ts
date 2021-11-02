1a25340bf9ab4762cfca68aeb4cf88a069836078
Josh Wright <jwwrightcalpoly@gmail.com>
2021-11-02 12:13:54 -0600

    draft of relational frame graph

--- apps/ui/src/app/block/digraph.ts
+++ apps/ui/src/app/block/digraph.ts
@@ -1,7 +1,7 @@
 /**
  * Represents a node in a graph.
  */
-import { StimuliComparisonTuple } from '../study-conditions/stimuli.interfaces';
+import { cloneDeep } from 'lodash-es';
 
 export class Node {
 
@@ -26,15 +26,40 @@ export class Node {
 
 }
 
+export class RelationalNode {
+  readonly name: string;
+  readonly network: number;
+  readonly value: string;
+
+  constructor(name: string, network: number, value = '') {
+    this.name = name;
+    this.network = network;
+    this.value = value;
+  }
+
+  /**
+   * Returns a printable representation of the given object. When the result
+   * from repr() is passed to eval(), we will get a copy of the original object
+   * @returns {string}
+   */
+  repr(): string {
+    return `new RelationalFrameNode('${this.name}','${this.network}','${this.value}')`;
+  }
+
+  toString(): string {
+    return `${this.name}${this.network}`;
+  }
+}
+
 /***
  * Represents an edge in the dictionary. Includes a source and a destination.
  */
-export class Edge {
+export class Edge<N extends Node> {
 
-  readonly dest: Node;
-  readonly src: Node;
+  readonly dest: N;
+  readonly src: N;
 
-  constructor(src: Node, dest: Node) {
+  constructor(src: N, dest: N) {
     this.dest = dest;
     this.src = src;
   }
@@ -55,19 +80,19 @@ export enum RelationType {
 /***
  * Represents an weighted edge in the dictionary. Includes a source and a destination.
  */
-export class RelationalEdge<T extends RelationType> extends Edge {
+export class RelationalEdge<T extends RelationType> extends Edge<RelationalNode> {
 
   readonly relation: string;
   readonly relationType: T;
 
-  constructor(src: Node, dest: Node, cue: string, relationType: T) {
+  constructor(src: RelationalNode, dest: RelationalNode, relation: string, relationType: T) {
     super(src, dest);
-    this.relation = cue;
+    this.relation = relation;
     this.relationType = relationType;
   }
 
   toString() {
-    return `${this.src}->${this.dest} (${this.relation}, ${this.relationType})`;
+    return `${this.src} ${this.relation} ${this.dest} (${this.relationType})`;
   }
 
 }
@@ -75,29 +100,29 @@ export class RelationalEdge<T extends RelationType> extends Edge {
 /***
  * Represents a directed graph of Node and Edge objects
  */
-export class DiGraph<T extends Edge> {
-  protected edges: Map<Node, T[]> = new Map<Node, T[]>();
-  protected nodes: Set<Node> = new Set();
+export class DiGraph<N extends Node, E extends Edge<N>> {
+  protected edges: Map<N, E[]> = new Map<N, E[]>();
+  protected nodes: Set<N> = new Set();
 
-  addEdge(edge: T) {
+  addEdge(edge: E) {
     const src = edge.src;
     const dest = edge.dest;
     if (!this.hasNode(src)) throw Error(`addEdge failed source node "${src.toString()}" is not in graph.`);
     if (!this.hasNode(dest)) throw Error(`addEdge failed destination node "${dest.toString()}" is not in graph.`);
-    this.edges.set(edge.src, (this.edges.get(edge.src) as T[]).concat(edge));
+    this.edges.set(edge.src, (this.edges.get(edge.src) as E[]).concat(edge));
   }
 
-  addNode(node: Node): void {
+  addNode(node: N): void {
     if (this.nodes.has(node)) throw Error(`addNode failed node "${node.toString()}" already exists`);
     this.nodes.add(node);
     this.edges.set(node, []);
   }
 
-  getEdgesForNode(node: Node): T[] {
+  getEdgesForNode(node: N): E[] {
     return this.edges.get(node) || [];
   }
 
-  hasNode(node: Node): boolean {
+  hasNode(node: N): boolean {
     return this.nodes.has(node);
   }
 
@@ -113,35 +138,94 @@ export class DiGraph<T extends Edge> {
 export interface StimuliComparisonGeneric {
   relation: string;
   relationType: string;
-  stimuli: StimuliComparisonTuple;
+  stimuli: [RelationalNode, RelationalNode];
 }
 
-export class RelationalFrameGraph extends DiGraph<RelationalEdge<RelationType>> {
+export class RelationalFrameGraph extends DiGraph<RelationalNode, RelationalEdge<RelationType>> {
 
-  reverseRelationDictionary: { [key: string]: string };
+  combinatorialDictionary: { [key: string]: { [key: string]: string }; };
+  reverseDictionary: { [key: string]: string };
   selfRelation: string;
   unknownRelation: string;
 
-  constructor(identityRelation: string, unknownRelation: string, reverseRelationDictionary: { [key: string]: string }) {
+  constructor(
+    selfRelation: string,
+    unknownRelation: string,
+    reverseDictionary: { [key: string]: string },
+    combinatorialDictionary: { [key: string]: { [key: string]: string }; }
+  ) {
     super();
-    this.selfRelation = identityRelation;
+    this.selfRelation = selfRelation;
     this.unknownRelation = unknownRelation;
-    this.reverseRelationDictionary = reverseRelationDictionary;
+    this.reverseDictionary = reverseDictionary;
+    this.combinatorialDictionary = combinatorialDictionary;
+  }
+
+  get combinatoriallyEntailed() {
+    const comparisons: StimuliComparisonGeneric[] = [];
+    const nodes = [...this.nodes];
+    for (const startNode of nodes) {
+      for (const endNode of nodes) {
+        if (startNode === endNode) continue;
+        if (this.getEdgesForNode(startNode).find(edge => edge.dest === endNode)) continue;
+        const relations = this.findPathway(startNode, endNode)
+          .map(path => path.edges.map(edge => edge.relation))
+          .reduce((acc, relations, i) => {
+            const relationsCopy = [...relations];
+
+            let reducedRelation = relationsCopy.shift() as string;
+
+            while (relationsCopy.length) {
+              const relation = relationsCopy.shift() as string;
+              reducedRelation = this.combinatorialDictionary[reducedRelation][relation];
+            }
+
+            acc.push(reducedRelation);
+
+            relations.length = 0;
+
+            return acc;
+          }, [] as string[]);
+
+        if (relations.length === 0) relations.push(this.unknownRelation);
+
+        if ([...new Set(relations.filter(relation => relation !== this.unknownRelation))].length > 1) {
+          console.warn(`Multiple relations detected for ${startNode.toString()} => ${endNode.toString()} comparison`,
+            relations);
+        } else {
+          comparisons.push({
+            relation: relations[0],
+            relationType: RelationType.combinatoriallyEntailed,
+            stimuli: [startNode, endNode]
+          });
+        }
+      }
+    }
+    return comparisons;
   }
 
   get identities(): StimuliComparisonGeneric[] {
     return [...this.nodes].map(node => ({
       relation: this.selfRelation,
       relationType: RelationType.identity,
-      stimuli: [node.name, node.name]
+      stimuli: [node, node]
     }));
+  }
+
+  get mutuallyEntailed(): StimuliComparisonGeneric[] {
+    return [...this.edges.values()].flat().filter(edge => edge.relationType === RelationType.mutuallyEntailed).map(
+      edge => ({
+        relation: edge.relation,
+        relationType: edge.relationType,
+        stimuli: [edge.src, edge.dest]
+      }));
   }
 
   get trained(): StimuliComparisonGeneric[] {
     return [...this.edges.values()].flat().filter(edge => edge.relationType === RelationType.trained).map(edge => ({
-      relation: this.selfRelation,
-      relationType: RelationType.trained,
-      stimuli: [edge.src.name, edge.dest.name]
+      relation: edge.relation,
+      relationType: edge.relationType,
+      stimuli: [edge.src, edge.dest]
     }));
   }
 
@@ -154,50 +238,53 @@ export class RelationalFrameGraph extends DiGraph<RelationalEdge<RelationType>> 
     const dest = edge.dest;
     if (!this.hasNode(src)) throw Error(`addEdge failed source node "${src.toString()}" is not in graph.`);
     if (!this.hasNode(dest)) throw Error(`addEdge failed destination node "${dest.toString()}" is not in graph.`);
-    if (!this.reverseRelationDictionary?.[edge.relation]) throw Error(
+    if (!this.reverseDictionary?.[edge.relation]) throw Error(
       `Could not find inverse of relation ${edge.relation}`);
     this.edges.set(edge.src, (this.edges.get(edge.src) as RelationalEdge<RelationType>[]).concat(edge));
     this.edges.set(edge.dest, (this.edges.get(edge.dest) as RelationalEdge<RelationType>[]).concat(
       new RelationalEdge(
         edge.dest,
         edge.src,
-        this.reverseRelationDictionary[edge.relation],
+        this.reverseDictionary[edge.relation],
         RelationType.mutuallyEntailed)));
   }
 
   findPathway(
-    startNode: Node,
-    endNode: Node,
-    path: Node[] = [],
-    paths: Node[][] = []
-  ): Node[][] {
-    path = [...path].concat(startNode);
+    startNode: RelationalNode,
+    endNode: RelationalNode,
+    edge?: RelationalEdge<RelationType>,
+    path: { nodes: RelationalNode[], edges: RelationalEdge<RelationType>[] } = { nodes: [], edges: [] },
+    paths: { nodes: RelationalNode[], edges: RelationalEdge<RelationType>[] }[] = []
+  ): { nodes: RelationalNode[], edges: RelationalEdge<RelationType>[] }[] {
+    path.nodes = path.nodes.concat(startNode);
+    if (edge) path.edges = path.edges.concat(edge);
     if (startNode === endNode) {
       return paths.concat(path);
     } else {
       for (const edge of this.getEdgesForNode(startNode)) {
-        // Avoid cycles
-        if (!path.includes(edge.dest) || !path.includes(startNode)) {
-          const newPath = this.findPathway(edge.dest, endNode, [...path], paths);
-          if (newPath.length) {
-            paths = paths.concat(newPath);
-          }
+
+        if (!path.nodes.map(node => node.name).includes(edge.dest.name))  // Avoid cycles
+        {
+          const newPath = this.findPathway(edge.dest, endNode, edge, cloneDeep(path), paths);
+          if (newPath) paths = paths.concat(newPath);
         }
       }
-      return [...paths];
+      return paths;
     }
   }
 
-  validate() {
-    const nodes = [[...this.nodes][0]];
-    for (const startNode of nodes) {
-      for (const endNode of nodes) {
-        if (startNode === endNode) continue;
-        this.findPathway(startNode, endNode);
-      }
-    }
+  toString(): string {
+    return ([] as StimuliComparisonGeneric[]).concat(
+        this.identities,
+        this.trained,
+        this.mutuallyEntailed,
+        this.combinatoriallyEntailed
+      )
+      .flat()
+      .map(
+        (stimuliComparison) => `${stimuliComparison.relationType}: ${stimuliComparison.stimuli[0]} ${stimuliComparison.relation} ${stimuliComparison.stimuli[1]}`).join(
+        '\n');
   }
-
 }
 
 // TESTS
--- apps/ui/src/app/home/home.component.ts
+++ apps/ui/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Node, RelationalEdge, RelationalFrameGraph, RelationType } from '../block/digraph';
+import { RelationalEdge, RelationalFrameGraph, RelationalNode, RelationType } from '../block/digraph';
 
 @Component({
   selector: 'home',
@@ -8,29 +8,74 @@ import { Node, RelationalEdge, RelationalFrameGraph, RelationType } from '../blo
 })
 export class HomeComponent {
   constructor() {
-    const graph = new RelationalFrameGraph('same', 'iCannotKnow', {
-      same: 'same',
-      lessThan: 'greaterThan',
-      greaterThan: 'lessThan',
-      iCannotKnow: 'iCannotKnow'
-    });
+    const combDict: { [key: string]: { [key: string]: string } } = {
+      greaterThan: {
+        same: 'greaterThan',
+        greaterThan: 'greaterThan',
+        lessThan: 'iCannotKnow',
+        iCannotKnow: 'iCannotKnow'
+      },
+      lessThan: {
+        same: 'lessThan',
+        greaterThan: 'iCannotKnow',
+        lessThan: 'lessThan',
+        iCannotKnow: 'iCannotKnow'
+      },
+      same: {
+        same: 'same',
+        greaterThan: 'greaterThan',
+        lessThan: 'lessThan',
+        iCannotKnow: 'iCannotKnow'
+      },
+      iCannotKnow: {
+        same: 'iCannotKnow',
+        greaterThan: 'iCannotKnow',
+        lessThan: 'iCannotKnow',
+        iCannotKnow: 'iCannotKnow'
+      }
+    };
+    const graph = new RelationalFrameGraph(
+      'same',
+      'iCannotKnow',
+      {
+        same: 'same',
+        lessThan: 'greaterThan',
+        greaterThan: 'lessThan',
+        iCannotKnow: 'iCannotKnow'
+      },
+      combDict);
 
-    const nodeA = new Node('A');
-    const nodeB = new Node('B');
-    const nodeC = new Node('C');
+    const nodeA1 = new RelationalNode('A', 1);
+    const nodeB1 = new RelationalNode('B', 1);
+    const nodeC1 = new RelationalNode('C', 1);
+    const nodeA2 = new RelationalNode('A', 2);
+    const nodeB2 = new RelationalNode('B', 2);
+    const nodeC2 = new RelationalNode('C', 2);
 
-    graph.addNode(nodeA);
-    graph.addNode(nodeB);
-    graph.addNode(nodeC);
+    graph.addNode(nodeA1);
+    graph.addNode(nodeB1);
+    graph.addNode(nodeC1);
+    graph.addNode(nodeA2);
+    graph.addNode(nodeB2);
+    graph.addNode(nodeC2);
 
-    graph.addTrainedAndMutualRelations(new RelationalEdge(nodeA, nodeB, 'same', RelationType.trained));
-    graph.addTrainedAndMutualRelations(new RelationalEdge(nodeA, nodeC, 'same', RelationType.trained));
+    graph.addTrainedAndMutualRelations(new RelationalEdge(nodeA1, nodeB1, 'same', RelationType.trained));
+    graph.addTrainedAndMutualRelations(new RelationalEdge(nodeB1, nodeC1, 'same', RelationType.trained));
+    graph.addTrainedAndMutualRelations(new RelationalEdge(nodeA1, nodeC1, 'same', RelationType.trained));
 
-    // console.log(graph.toString());
-    // console.log(graph.identities);
-    // console.log(graph.trained);
+    graph.addTrainedAndMutualRelations(new RelationalEdge(nodeA2, nodeB2, 'greaterThan', RelationType.trained));
+    graph.addTrainedAndMutualRelations(new RelationalEdge(nodeB2, nodeC2, 'greaterThan', RelationType.trained));
+    graph.addTrainedAndMutualRelations(new RelationalEdge(nodeA2, nodeC2, 'greaterThan', RelationType.trained));
 
-    console.log('pathways', graph.findPathway(nodeB, nodeC))
+    console.log(graph.toString());
+
+    // const pathRelationSets = graph.findPathway(nodeA, nodeC)
+    //   .map(path => {
+    //     console.log(path)
+    //   })
+
+    // graph.validate();
+
   }
 }
 
