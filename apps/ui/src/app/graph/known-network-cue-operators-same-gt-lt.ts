b6b67f5ba7be24883dc974511ba22c9267e34af5
Josh Wright <jwwrightcalpoly@gmail.com>
2021-11-10 09:16:47 -0700

    remove constraints from known network operators

--- apps/ui/src/app/graph/known-network-cue-operators-same-gt-lt.ts
+++ apps/ui/src/app/graph/known-network-cue-operators-same-gt-lt.ts
@@ -7,13 +7,13 @@ export type TriNodeNetworkOperatorCombination<T> = [aToB: T|undefined, aToC: T|u
  * @type {(["same", "same", undefined] | ["same", "greaterThan", undefined] | ["same", "lessThan", undefined] | ["lessThan", "same", undefined] | ["lessThan", "lessThan", "lessThan"] | ["lessThan", "greaterThan", undefined] | ["greaterThan", "same", undefined] | ["greaterThan", "lessThan", undefined] | ["greaterThan", "greaterThan", "greaterThan"])[]}
  */
 export const KNOWN_NETWORK_CUE_OPERATORS_SAME_GT_LT: TriNodeNetworkOperatorCombination<CueNonArbitrary>[] = [
-  // [CUE_NON_ARBITRARY.same, CUE_NON_ARBITRARY.same, undefined],
-  // [CUE_NON_ARBITRARY.same, CUE_NON_ARBITRARY.greaterThan, undefined],
-  // [CUE_NON_ARBITRARY.same, CUE_NON_ARBITRARY.lessThan, undefined],
-  // [CUE_NON_ARBITRARY.lessThan, CUE_NON_ARBITRARY.same, undefined],
+  [CUE_NON_ARBITRARY.same, CUE_NON_ARBITRARY.same, undefined],
+  [CUE_NON_ARBITRARY.same, CUE_NON_ARBITRARY.greaterThan, undefined],
+  [CUE_NON_ARBITRARY.same, CUE_NON_ARBITRARY.lessThan, undefined],
+  [CUE_NON_ARBITRARY.lessThan, CUE_NON_ARBITRARY.same, undefined],
   [CUE_NON_ARBITRARY.lessThan, undefined, CUE_NON_ARBITRARY.lessThan],
-  // [CUE_NON_ARBITRARY.lessThan, CUE_NON_ARBITRARY.greaterThan, undefined],
-  // [CUE_NON_ARBITRARY.greaterThan, CUE_NON_ARBITRARY.same, undefined],
-  // [CUE_NON_ARBITRARY.greaterThan, CUE_NON_ARBITRARY.lessThan, undefined],
+  [CUE_NON_ARBITRARY.lessThan, CUE_NON_ARBITRARY.greaterThan, undefined],
+  [CUE_NON_ARBITRARY.greaterThan, CUE_NON_ARBITRARY.same, undefined],
+  [CUE_NON_ARBITRARY.greaterThan, CUE_NON_ARBITRARY.lessThan, undefined],
   [CUE_NON_ARBITRARY.greaterThan, undefined, CUE_NON_ARBITRARY.greaterThan]
 ];
