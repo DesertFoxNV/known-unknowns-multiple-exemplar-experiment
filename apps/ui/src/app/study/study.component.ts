59a19b593355d8cecf30e2f8cd9c81770a24c263
Joshua Wright <jwwrightcalpoly@gmail.com>
2022-01-08 16:46:30 -0800

    add flag to prevent abandon after a completed dialog is shown

--- apps/ui/src/app/study/study.component.ts
+++ apps/ui/src/app/study/study.component.ts
@@ -1,27 +1,28 @@
-import { ComponentType } from '@angular/cdk/overlay';
-import { DOCUMENT } from '@angular/common';
-import { Component, ComponentFactoryResolver, Inject, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { fromEvent, timer } from 'rxjs';
-import { filter, first, switchMap, tap } from 'rxjs/operators';
+import {ComponentType} from '@angular/cdk/overlay';
+import {DOCUMENT} from '@angular/common';
+import {Component, ComponentFactoryResolver, Inject, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
+import {fromEvent, timer} from 'rxjs';
+import {filter, first, switchMap, tap} from 'rxjs/operators';
 import {
-  BlockButtonDialogComponent, BlockButtonDialogData
+  BlockButtonDialogComponent,
+  BlockButtonDialogData
 } from '../block/block-button-dialog/block-button-dialog.component';
-import { BlockComponent } from '../block/block.component';
-import { ForcedChoiceBlockComponent } from '../block/forced-choice-block-component/forced-choice-block.component';
-import { fullScreenDialogWithData } from '../block/full-screen-dialog-with-data';
-import { OperantChoiceBlockComponent } from '../block/operant-choice-block-component/operant-choice-block.component';
-import { PreTestBlockComponent } from '../block/pre-test-block-component/pre-test-block.component';
+import {BlockComponent} from '../block/block.component';
+import {ForcedChoiceBlockComponent} from '../block/forced-choice-block-component/forced-choice-block.component';
+import {fullScreenDialogWithData} from '../block/full-screen-dialog-with-data';
+import {OperantChoiceBlockComponent} from '../block/operant-choice-block-component/operant-choice-block.component';
+import {PreTestBlockComponent} from '../block/pre-test-block-component/pre-test-block.component';
 import {
   TrainingNetworksBlockComponent
 } from '../block/training-networks-block-component/training-networks-block.component';
-import { TRIAL_DELAY_INTERVAL_MS } from '../block/trial-animation-delay';
-import { ReportStatus } from '../report/report-status';
-import { ReportService } from '../report/report.service';
-import { StudyConfigService } from '../study-config-form/study-config.service';
-import { SurveyService } from '../survey/survey.service';
-import { STUDY_INSTRUCTIONS } from './study-instructions';
+import {TRIAL_DELAY_INTERVAL_MS} from '../block/trial-animation-delay';
+import {ReportStatus} from '../report/report-status';
+import {ReportService} from '../report/report.service';
+import {StudyConfigService} from '../study-config-form/study-config.service';
+import {SurveyService} from '../survey/survey.service';
+import {STUDY_INSTRUCTIONS} from './study-instructions';
 
 @UntilDestroy()
 @Component({
@@ -38,9 +39,10 @@ export class StudyComponent implements OnInit {
     TrainingNetworksBlockComponent
   ];
   complete = false;
-  @ViewChild('container', { read: ViewContainerRef, static: true }) container?: ViewContainerRef;
+  @ViewChild('container', {read: ViewContainerRef, static: true}) container?: ViewContainerRef;
   instructions = STUDY_INSTRUCTIONS;
   showInstructions = true;
+  completeDialogShown = false;
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
@@ -60,7 +62,7 @@ export class StudyComponent implements OnInit {
     const blockInstance = componentRef.instance;
     blockInstance.studyConfig = this.studyConfigSvc.config;
 
-    blockInstance.completed.pipe(first(), tap(({ failed }) => {
+    blockInstance.completed.pipe(first(), tap(({failed}) => {
       if (failed) {
         this.showCompleteDialog('failed');
       } else if (this.blocks.length) {
@@ -85,7 +87,8 @@ export class StudyComponent implements OnInit {
   }
 
   showCompleteDialog(status: ReportStatus) {
-    const { participantId } = this.studyConfigSvc;
+    this.completeDialogShown = true;
+    const {participantId} = this.studyConfigSvc;
     const text = status === 'abandoned' ? `STUDY ABANDONED!\n\n PARTICIPANT ID:\n ${participantId}` :
       `THANKS FOR PARTICIPATING!\n\n PARTICIPANT ID:\n ${participantId}`;
     this.complete = true;
@@ -98,19 +101,19 @@ export class StudyComponent implements OnInit {
         return this.reportSvc.sendReport(status);
       }),
       switchMap(() => this.dialog.open(BlockButtonDialogComponent,
-        fullScreenDialogWithData<BlockButtonDialogData>({ text, disableClose: true })).afterClosed())
+        fullScreenDialogWithData<BlockButtonDialogData>({text, disableClose: true})).afterClosed())
     ).subscribe();
   }
 
   showPreSurvey() {
     this.surveySvc.showPreSurvey(this.studyConfigSvc.participantId).pipe(tap(() => {
       fromEvent(this.document, 'visibilitychange').pipe(
-        filter(() => this.document.hidden),
-        first(),
+        filter(() => this.document.hidden && !this.completeDialogShown),
         tap(() => {
           this.container?.clear();
           this.showCompleteDialog('abandoned');
-        })
+        }),
+        untilDestroyed(this)
       ).subscribe();
     })).subscribe(() => this.nextBlock());
   }
